b889c31428a27cd2181eab6cfca39332d6ec17f5
Kaitlyn V <kvenzen99@gmail.com>
2025-11-23 15:58:40 -0500

    Added accounts, learning_logs, and ll_project

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -3,18 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
-    """A topic the user is learning about """
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    
     def __str__(self):
-        """Return a string representation of the model."""
         return self.text
 
-class Entry (models.Model):
-    """Something specific learned about a topic."""
+class Entry(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
@@ -23,5 +19,4 @@ class Entry (models.Model):
         verbose_name_plural = 'entries'
 
     def __str__(self):
-        """Return a string representation of the model."""
-        return self.text[:50] + "..."
+        return f"{self.text[:50]}..."
